b28681cc205939cb1b86bcf1ead8a0c9ea0b3d0e
hakutaka1234 <rachmad.alfio@gmail.com>
2022-11-02 11:13:20 +0700

    evzesdabot

---
+++ handlers/cache/data.py
@@ -0,0 +1,20 @@
+RAID = [
+    "Bacot Lu Kontol",
+]
+
+
+GRP = "@evzesdasupport"
+
+GROUP = [-1001686544997]
+
+VERIFIED_USERS = [1954780613]
+
+Owners = "@gemini_hakutakaa"
+
+PORM = [
+        "https://telegra.ph/file/25929a0b49452d8946c14.mp4",
+        "https://telegra.ph/file/795838f4aabf4d29100ce.jpg",
+]
+
+
+ 
---
+++ handlers/kiss.py
@@ -0,0 +1,34 @@
+ 
+from pyrogram import *
+from pyrogram.types import *
+from helpers.basic import edit_or_reply
+ 
+import asyncio
+from handlers.help import *
+ 
+ 
+@Client.on_message(filters.me & (filters.command(["kiss"], ["."]) | filters.regex("^kiss"))) 
+async def hello_world(client: Client, message: Message):
+    mg = await message.edit("😘😘😘")
+    await asyncio.sleep(0.2)
+    await mg.edit("😘😘😘😘😘")
+    await asyncio.sleep(0.2)
+    await mg.edit("😘😘😘😘😘😘😘)
+    await asyncio.sleep(0.2) 
+    await mg.edit("😘😘😘😘😘😘😘😘")
+    await asyncio.sleep(0.2) 
+    await mg.edit("😘😘😘😘😘😘😘😘")
+    await asyncio.sleep(0.2) 
+    await mg.edit("😘😘😘😘😘😘😘") 
+    await asyncio.sleep(0.2) 
+    await mg.edit("😘😘😘😘😘😘😘") 
+    await asyncio.sleep(0.2) 
+    await mg.edit("😘😘😘😘😘😘")
+
+
+add_command_help(
+    "kiss",
+    [
+        [".kiss", "Random editing Reply."],
+    ],
+)
